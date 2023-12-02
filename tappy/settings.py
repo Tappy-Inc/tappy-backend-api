2b4383434ea2c0a79240124332c428fb8139dbfb
Joven Tan <jovenntann@gmail.com>
2023-12-02 19:16:51 +0800

    feat(user): add user domain

--- tappy/settings.py
+++ tappy/settings.py
@@ -55,6 +55,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Domain
+    'domain.user'
+    # API
 ]
 
 MIDDLEWARE = [
