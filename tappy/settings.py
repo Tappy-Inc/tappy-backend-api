c46b3efacac1ab156df46fc2849bf1709537e9a7
Joven Tan <jovenntann@gmail.com>
2023-12-05 03:50:47 +0800

    fix: update health check version to 1.0.6

--- tappy/settings.py
+++ tappy/settings.py
@@ -240,6 +240,7 @@ SWAGGER_SETTINGS = {
 }
 
 # Library: django-cors-headers
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     'https://hris.tappy.com.ph'
