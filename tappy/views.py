91dd73d5c7bf56d2e87210f045b65b95607a48fb
Joven Tan <jovenntann@gmail.com>
2023-12-05 15:27:06 +0800

    feat: Update session cookie settings and API version number

--- tappy/views.py
+++ tappy/views.py
@@ -23,6 +23,6 @@ def health_check_view(request):
     content = {
         "status_code": 200,
         "status": "OK",
-        "version": "1.0.15"
+        "version": "1.0.16"
     }
     return Response(content)
