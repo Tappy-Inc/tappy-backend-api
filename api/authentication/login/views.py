c832a16b1a830599bdf8a612df8030d413143191
Joven Tan <jovenntann@gmail.com>
2023-12-05 20:33:11 +0800

    feat: Set cookies for multiple domains

--- api/authentication/login/views.py
+++ api/authentication/login/views.py
@@ -63,9 +63,13 @@ class AuthenticationLoginAPIView(APIView):
             # NOTE: Do not use data=data if mixing with Model Serializers
             credential_serializer = ReadCredentialSerializer(data)
             response = Response(credential_serializer.data)
-            # Set the session key in the cookies
+            # Set cookie for .tappy.com.ph
             response.set_cookie('sessionid', session_key, max_age=86400, domain=".tappy.com.ph", samesite='None', secure=True)
             response.set_cookie(session.session_key, session.session_data, max_age=86400, domain=".tappy.com.ph", samesite='None', secure=True)
+
+            # Set cookie for localhost:3000
+            response.set_cookie('sessionid', session_key, max_age=86400, domain="localhost:3000", samesite='None', secure=True)
+            response.set_cookie(session.session_key, session.session_data, max_age=86400, domain="localhost:3000", samesite='None', secure=True)
             return response
         else:
             error_serializer = ErrorDetailSerializer(data={'detail': 'No active account found with the given credentials'})
