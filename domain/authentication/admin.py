a743d24a3b72c9d9b93aba03469019d5f192c119
Joven Tan <jovenntann@gmail.com>
2023-12-08 21:32:38 +0800

    feat(admin): add ModelAdmin to SessionAdmin

--- domain/authentication/admin.py
+++ domain/authentication/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from domain.authentication.models.Session import Session
+from unfold.admin import ModelAdmin
 
 # Register your models here.
 
 @admin.register(Session)
-class SessionAdmin(admin.ModelAdmin):
+class SessionAdmin(ModelAdmin):
     list_display = ('id', 'user', 'session_key', 'session_data', 'expire_date')
     search_fields = ('user', 'session_key',)
