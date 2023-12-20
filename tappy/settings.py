be9e062c6553f1217cc1cc65c81d0378f218e6a0
Joven Tan <jovenntann@gmail.com>
2023-12-20 21:41:31 +0800

    Update ALLOWED_HOSTS to use environment variable in settings.py

--- tappy/settings.py
+++ tappy/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
 
