615e4c065cf8a3f024d1f10ab8498edb6be02689
Joven Tan <jovenntann@gmail.com>
2023-12-21 23:03:39 +0800

    update Document model

--- domain/user/models/Document.py
+++ domain/user/models/Document.py
@@ -9,11 +9,14 @@ logger = logging.getLogger(__name__)
 
 class Document(BaseModel):
     
+    def user_directory_path(self, filename):
+        return f"user_{self.user.id}/documents/{filename}"
+
     user = models.ForeignKey(User, related_name='documents', on_delete=models.CASCADE)
     file_name = models.CharField(max_length=255)
     file_type = models.CharField(max_length=255)
     file_source = models.CharField(max_length=255)
-    file_upload = models.FileField(upload_to='documents/')
+    file_upload = models.FileField(upload_to=user_directory_path)
     file_size = models.IntegerField()
     history = HistoricalRecords()
 
